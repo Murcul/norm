45ddc63fe5f37242b6e6a0d52d1ad3e3fcc44be9
Maciej Siwek <m@tooinconsistent.com>
2023-03-01 16:50:19 +0100

    drop norm code in

---
+++ README.md
@@ -0,0 +1,3 @@
+# norm - not an orm
+
+Norm is a library to perform sql queries for syncing operations.
---
+++ helpers.ts
@@ -0,0 +1,3 @@
+export const quoteAndJoin = (list: Array<string | number | symbol>): string => {
+  return list.map(el => `"${String(el)}"`).join(', ');
+};
