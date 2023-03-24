8c38219d7f6cdcb07df60d4d14319b7bcd8cada0
Miracle Toluwase Ogunlade <toluwase103@gmail.com>
2023-03-24 11:55:45 +0100

    Update package.json

--- src/build_npm.ts
+++ src/build_npm.ts
@@ -1,7 +1,6 @@
-import { dnt, copy } from './dev_deps.ts';
+import { dnt } from './dev_deps.ts';
 
-await Deno.remove("npm", { recursive: true }).catch((_) => {});
-await copy("patches", "npm/patches", { overwrite: true });
+await Deno.remove('npm', { recursive: true }).catch((_) => {});
 
 await dnt.build({
   entryPoints: [
@@ -15,8 +14,13 @@ await dnt.build({
   outDir: './npm',
   skipSourceOutput: true,
   shims: {
-    crypto: true,
     deno: true,
+    custom: [
+      {
+        module: './src/shims/crypto.ts',
+        globalNames: ['crypto'],
+      },
+    ],
   },
   typeCheck: false,
   test: false,
@@ -26,14 +30,8 @@ await dnt.build({
     version: Deno.args[0]?.replace(/^v/, ''),
     description: '<discription>',
     license: 'MIT',
-    scripts: {
-      "postinstall": "patch-package"
-    },
     bin: {
-      norm: "./script/cli.js"
+      norm: './script/cli.js',
     },
-    dependencies: {
-      "patch-package": "^6.5.1",
-    }
   },
 });
---
+++ src/shims/crypto.ts
@@ -0,0 +1,4 @@
+// deno-lint-ignore no-external-import
+import crypto from 'https://deno.land/std@0.110.0/node/crypto.ts';
+
+export { crypto };
